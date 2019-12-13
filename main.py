abf64f1304a277e192b24634810cfe93c749c285
Anton <anton.lagosha.99@mail.ru>
2019-12-13 21:45:35 +0500

    resources with styles added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.Qt import QTranslator, QLocale
+from PyQt5.Qt import QTranslator, QLocale, QFile, QIODevice, QByteArray
 from mainwindow import MainWindow
+import res.resources
 
 
 def main():
 
     app = QApplication(sys.argv)
+
+    styles = QFile(":/style/styles.qss")
+    styles.open(QIODevice.ReadOnly)
+
+    app.setStyleSheet(styles.readAll().data().decode("latin1"))
+
     translator = QTranslator()
     if translator.load(QLocale(), "localization", "_", "localization"):
         app.installTranslator(translator)
